b608e342078401fe57720857a7c894a6c507e41a
Michael G. Morey <mgmorey@gmail.com>
2021-02-09 19:12:51 -0600

    Print "Hello, World!" by invoking printf().

--- hello.asm
+++ hello.asm
@@ -3,18 +3,16 @@
 section .data
         NUL     equ     0x00
         LF      equ     0x0a
-        msg     db      "Hello, World!", LF, NUL
-        len     equ     $-msg-1
+        fmt     db      "Hello, World!", LF, NUL
 section .bss
 section .text
+        extern  printf
         global  main
 main:   push    rbp             ; prologue
         mov     rbp,    rsp     ; prologue
-        mov     rax,    1       ; 1 = write
-        mov     rdi,    1       ; 1 = to stdout
-        mov     rsi,    msg     ; message to print
-        mov     rdx,    len     ; length of message w/o trailing NUL
-        syscall                 ; print the message
+        mov     rax,    1       ; 1 argument
+        mov     rdi,    fmt     ; format specifier
+        call    printf          ; call printf()
         mov     rax,    0       ; set return code
         mov     rsp,    rbp     ; epilogue
         pop     rbp             ; epilogue
